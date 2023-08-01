7d99884299ee1db3329f02150788d74a8d6efe41
jvanpinchuk <j.v.a.n.pinchuk@gmail.com>
2023-08-01 10:14:59 +0300

    API test added/ Check of tracker type

--- tests/test_api_booking.py
+++ tests/test_api_booking.py
@@ -1,4 +1,4 @@
-import time, allure, pytest, requests, json
+import time, allure, pytest, requests, json, re
 from page_object.api_data import Url, Headers
 
 
@@ -8,6 +8,18 @@ def test_check_status_code_get():
     response = requests.request("GET", Url.url, headers=Headers.headers)
     with allure.step('Check of status code'):
         assert response.status_code == 200
+        assert bool(re.search("tracker_type: 'Client'", response.text)), ('The searched text was not '
+                                                                          'found in the response.')
+
+
+@allure.suite("Check API of booking.com")
+@allure.title("Check of tracker type is Client")
+def test_check_tracker_type():
+    response = requests.request("GET", Url.url, headers=Headers.headers)
+    with allure.step('Check of tracker type is Client'):
+        assert bool(re.search("tracker_type: 'Client'", response.text)), ('The searched text was not '
+                                                                          'found in the response.')
+
 
 
 
